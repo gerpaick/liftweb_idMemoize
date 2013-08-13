d64029b728b7f3f731b22d8b348b91ba9b2c5fb4
Gerard Rakoczy <gerard.rakoczy@kagdev.pl>
2013-08-13 14:58:24 +0200

    gen invoices

--- src/main/scala/code/lib/Util.scala
+++ src/main/scala/code/lib/Util.scala
@@ -8,22 +8,18 @@ object Util extends Logger {
   def generateInvoices {
 
     if (code.model.Invoice.count.toInt == 0) {
-      //create some invoices
-      for (i <- 1 to 5) {
         val inv: code.model.Invoice = code.model.Invoice.create
-        inv.name("Invoice #" + i).status("Open").save()
-        info("Invoice #: " + i + " added.")
-      }
-      for (i <- 1 to 250) {
+        inv.name("Invoice #1").status("Open").save()
+        info("Invoice #1 added.")
+
+      for (i <- 1 to 10) {
         val item: code.model.Item = code.model.Item.create
 
         var qty = new scala.util.Random().nextInt(10)
         if (qty == 0) qty = qty + 1
-        var invId = new scala.util.Random().nextInt(5)
-        if (invId == 0) invId = invId + 1
         val price = new scala.util.Random().nextDouble() * 10.0d
 
-        item.name("Item #" + i).quantity(qty).invoice(invId).price(price).save()
+        item.name("Item #" + i).quantity(qty).invoice(1).price(price).save()
         info("Item #: " + i + " added to invoice #" + invId)
       }
     }
